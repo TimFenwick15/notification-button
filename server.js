b75be5d752c26ced1f1c6a889100d0e454279495
Tim Fenwick <fent@ncode.com>
2017-11-07 14:45:20 +0000

    Added a DB

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 let notifyStatus = false
+
+const sqlite3 = require('sqlite3').verbose();
+//const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database('mydb.db');
+// does this die when heroku goes down?
+
 const respond = res => {
   res.write(`Ready: ${notifyStatus}`)
   res.end()
@@ -14,4 +20,18 @@ app.get('/received', (req, res) => {
   notifyStatus = false
   respond(res)
 })
-app.listen(80) // localhost/ready
+
+// initialise db
+db.on('open', function() {
+  db.run("CREATE TABLE status (pendingnotification INTEGER)", function(err) {
+    if (err) console.error(err)
+    db.run("INSERT INTO status(pendingnotification) VALUES(0)", function(err) {
+      if (err) console.error(err)
+      db.all("SELECT * FROM status", function(err, rows) {
+        if (err) console.error(err)
+        console.log(rows)
+      })
+      app.listen(80) // localhost/ready
+    })
+  })
+})
